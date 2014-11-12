d4ee6c523074850f2886f2a8dd4fb18e16e3468b
David Bradway <david.bradway@gmail.com>
2014-11-12 16:11:05 -0500

    add a blink script (modeled on Arduino), test GPIO relay

---
+++ blink.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+"""
+blink.py
+
+Blink an output (relay)
+
+=======
+run with:
+    sudo ./blink.py
+
+Copyright 2014 David P. Bradway (dpb6@duke.edu)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+__author__ = "David Bradway"
+__email__ = "dpb6@duke.edu"
+__license__ = "Apache v2.0"
+
+import Adafruit_BBIO.GPIO as GPIO
+import time
+
+def setup():
+    GPIO.setup("P9_11", GPIO.OUT)
+
+def loop():
+    GPIO.output("P9_11", GPIO.HIGH)
+    time.sleep(10.0)
+    GPIO.output("P9_11", GPIO.LOW)
+    time.sleep(0.5)
+
+if __name__ == "__main__":
+    setup()
+    while True:
+        loop()
